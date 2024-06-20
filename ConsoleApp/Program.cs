ed88e7af67e9f1434a6ed42e97244e382f787ddb
Azman Ahmed <ahmed.azman.bd0007@gmail.com>
2024-06-20 18:57:31 +0600

    20th June, 2024

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -6,6 +6,20 @@ class Program
 {
     static void Main()
     {
+        PrintLine();
+        ReadAndWrite();
+    }
+
+    static void PrintLine()
+    {
         Console.WriteLine("Hello World");
+    }
+
+    static void ReadAndWrite()
+    {
+        Console.WriteLine("Enter Your Name");
+        string FirstName = Console.ReadLine();
+        string LastName = Console.ReadLine();
+        Console.WriteLine("Hello {0} {1}", FirstName, LastName);
     }
 }
